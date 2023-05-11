72d4431b9ea7f4eed8fc1c0e0d37752bf9fc56d1
Olitoni <olitoniyt@gmail.com>
2023-05-11 14:29:51 +0200

    lista27

---
+++ lista27/main.js
@@ -0,0 +1,37 @@
+function kalkuluj(obj) {
+  let action = obj.innerText;
+  switch (action) {
+    case 'Wyczyść': {
+      czysc();
+      break;
+    }
+
+    case 'Przelicz': {
+      let liczba = document.querySelector('#liczba').value;
+      let podstawa = document.querySelector('#podstawa').value.replace(',','.');
+      document.querySelector('#wynik').value = liczba * podstawa;
+      break;
+    }
+    case '+': {
+      let liczba = +document.querySelector('#liczba').value;
+      document.querySelector('#liczba').value = liczba + 1;
+      break;
+    }
+    case '-': {
+      let liczba = +document.querySelector('#liczba').value;
+      document.querySelector('#liczba').value = liczba - 1;
+      break;
+    }
+
+    default: {
+      let liczba = document.querySelector('#liczba').value + '';
+      document.querySelector('#liczba').value = liczba + action;
+    }
+  }
+}
+
+function czysc() {
+  document.querySelector('#liczba').value = '';
+  document.querySelector('#podstawa').value = '';
+  document.querySelector('#wynik').value = '';
+}
