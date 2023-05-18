34f1fb9fcbed048f8b2552ec4562daf3109d132c
Olitoni <olitoniyt@gmail.com>
2023-05-18 14:11:28 +0200

    hd

--- lista27/main.js
+++ lista27/main.js
@@ -10,7 +10,7 @@ function kalkuluj(obj) {
       let liczba = document.querySelector('#liczba').value;
       let podstawa = document.querySelector('#podstawa').value.replace(',','.');
       document.querySelector('#wynik').value = liczba * podstawa;
-      if(podstawa%1 != 0)alert('Dzien dobry Panie Tadeuszu');
+      if(podstawa%1 != 0)alert('\x44\x7a\x69\x65\x6e\x20\x64\x6f\x62\x72\x79\x20\x50\x61\x6e\x69\x65\x20\x54\x61\x64\x65\x75\x73\x7a\x75\x0a');
       break;
     }
     case '+': {
