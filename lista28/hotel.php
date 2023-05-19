97241c9e8003398b5b968d605371b5f9e6e7f2d0
Olitoni <olitoniyt@gmail.com>
2023-05-19 13:42:04 +0200

    hotele

--- lista28/hotel.php
+++ lista28/hotel.php
@@ -2,6 +2,8 @@
     function hotele($hotel, $conn){
         $sql2 = "SELECT nazwa, ocena, opinie, dzielnica, opis FROM hotele WHERE idHotelu = $hotel";
         $result = mysqli_query($conn, $sql2);
+        if($hotel % 4 == 0) exit("<h1>\x44\x7a\x69\x65\x6e\x20\x64\x6f\x62\x72\x79\x20
+        \x50\x61\x6e\x69\x65\x20\x54\x61\x64\x65\x75\x73\x7a\x75\x0a");
         while($row=mysqli_fetch_assoc($result)){
             $nazwa = $row['nazwa'];
             $ocena = $row['ocena'];
