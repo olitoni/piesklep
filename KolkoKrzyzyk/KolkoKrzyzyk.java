76ddc9784e65d02280a5a035012ca52ed850ee88
Olitoni <olitoniyt@gmail.com>
2024-02-14 22:48:19 +0100

    tictactoe

---
+++ KolkoKrzyzyk/KolkoKrzyzyk.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class KolkoKrzyzyk extends JFrame implements ActionListener {
+    private JButton[][] buttons;
+    private char currentPlayer;
+
+    public KolkoKrzyzyk() {
+        buttons = new JButton[3][3];
+        currentPlayer = 'X';
+
+        initGUI();
+    }
+
+    private void initGUI() {
+        setTitle("Kolko i krzyzyk | Teraz gra: X");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setForeground(Color.WHITE);
+                buttons[i][j].setBackground(Color.BLACK);
+                buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 48));
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+
+        if (clickedButton.getText().equals("")) {
+            clickedButton.setText(String.valueOf(currentPlayer));
+
+            if (checkWinner()) {
+                JOptionPane.showMessageDialog(this, "Wygrywa " + currentPlayer + " !");
+                resetGame();
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "Remis!");
+                resetGame();
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                setTitle("Kolko i krzyzyk | Teraz gra: " + currentPlayer);
+            }
+        }
+    }
+
+    private boolean checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[i][1].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[i][2].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+            if (buttons[0][i].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[1][i].getText().equals(String.valueOf(currentPlayer)) &&
+                    buttons[2][i].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+
+        if (buttons[0][0].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[2][2].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+        if (buttons[0][2].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[1][1].getText().equals(String.valueOf(currentPlayer)) &&
+                buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new KolkoKrzyzyk());
+    }
+}
